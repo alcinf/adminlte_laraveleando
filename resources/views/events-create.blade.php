05a4b75e03b12782d08174f51966f237e509b561
alcinf <alcinf@gmail.com>
2024-10-01 12:28:45 -0600

    02. Add Date picker.

--- resources/views/events-create.blade.php
+++ resources/views/events-create.blade.php
@@ -27,8 +27,7 @@
             </x-adminlte-select>
         </div>
         <div class="row"> 
-            <x-adminlte-select2 name="type" label="Tipo"
-                                data-placeholder="Select" fgroup-class="col-md-6">
+            <x-adminlte-select2 name="type" label="Type" data-placeholder="Select" fgroup-class="col-md-6">
                 <x-slot name="prependSlot">
                     <div class="input-group-text bg-gradient-info">
                         <i class="fas fa-building"></i>
@@ -44,6 +43,18 @@
  
             </x-adminlte-select2>
  
+        </div>
+        <div class="row">
+            @php
+                $config = ['format' => 'YYYY-MM-DD'];
+            @endphp
+            <x-adminlte-input-date name="date" label="Date" :config="$config" placeholder="Choose a date" fgroup-class="col-md-6">
+                <x-slot name="prependSlot">
+                    <div class="input-group-text bg-gradient-info">
+                        <i class="fas fa-calendar"></i>
+                    </div>
+                </x-slot>
+            </x-adminlte-input-date>
         </div>
         <div class="row">
             <div class="form-group col-md-6">
